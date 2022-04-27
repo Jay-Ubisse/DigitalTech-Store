7f9e3cf677224d5cc9839c304101268a0fa63d48
Jay Ubisse <ubisse.joaquim@outlook.com>
2022-04-27 12:41:06 +0200

    adding methods to authentiate user and admin info

--- src/digitaltech/Roles.java
+++ src/digitaltech/Roles.java
@@ -1,1 +1,31 @@
 package digitaltech;
+
+public class Roles {
+    private String userName;
+    private String userPassword;
+    private String adminName;
+    private String adminPassword;
+
+    public Roles() {
+        this.userName = "user";
+        this.userPassword = "s3cr3t";
+        this.adminName = "admin";
+        this.adminPassword = "s3cr3t0";
+    }
+
+    public boolean verifyUserPassword(String password) {
+        return (this.userPassword.equals(password));
+    }
+
+    public boolean verifyAdminPassword(String password) {
+        return (this.adminPassword.equals(password));
+    }
+
+    public boolean verifyUserName(String name) {
+        return (this.userName.equals(name));
+    }
+
+    public boolean verifyAdminName(String name) {
+        return (this.adminName.equals(name));
+    }
+}
